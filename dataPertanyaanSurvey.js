e19b58834c7c9f8df800ecc8a2f4b42ce01522a8
lazyudhowd <154648790+lazyudhowd@users.noreply.github.com>
2023-12-26 16:40:37 +0700

    Update dataPertanyaanSurvey.js
    Penambahan beberapa data
    Saat ini data lebih sesuai mengikuti contoh format yang ada di lembar survey lazismu kota singkawang saat ini

--- dataPertanyaanSurvey.js
+++ dataPertanyaanSurvey.js
@@ -4,7 +4,7 @@ let dataSurvey = {
 	versi : "1.0",
 	subSurvey : [
 		{
-			judulSub:"Sub Judul 1",
+			judulSub:"DATA KELUARGA PENERIMA MANFAAT",
 			pertanyaanSurvey : [
 				{
 					label : "Pendidikan Terakhir",
@@ -27,203 +27,426 @@ let dataSurvey = {
 					]
 				},
 				{
-					label : "Pertanyaan 3, sub 1",
+					label : "Usia Kepala Keluarga",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 3 Sub 1"},
-						{nilai:"2", label:"Opsi B Pertanyaan 3 Sub 1"},
-						{nilai:"3", label:"Opsi C Pertanyaan 3 Sub 1"},
-						{nilai:"4", label:"Opsi D Pertanyaan 3 Sub 1"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 3 Sub 1"}
+						{nilai:"5", label:"50-60"},
+						{nilai:"4", label:"30-45"},
+						{nilai:"3", label:"25-35"},
+						{nilai:"2", label:"15-25"}
+					]
+				},
+				{
+					label : "Status Marital",
+					opsiJawaban : [
+						{nilai:"5", label:"Menikah"},
+						{nilai:"4", label:"Janda / Duda"},
+						{nilai:"3", label:"Gadis / Perjaka"}
+					]
+				},
+				{
+					label : "Kondisi Kepala Keluraga",
+					opsiJawaban : [
+						{nilai:"5", label:"Sakit Menahun"},
+						{nilai:"4", label:"Sakit-sakitan"},
+						{nilai:"3", label:"Sehat & tidak bekerja"},
+						{nilai:"2", label:"Sehat dan Bekerja"}
+					]
+				},
+				{
+					label : "Jumlah Tanggungan",
+					opsiJawaban : [
+						{nilai:"5", label:"4 Anak"},
+						{nilai:"4", label:"3 Anak"},
+						{nilai:"3", label:"2 Anak"},
+						{nilai:"2", label:"1 Anak"}
+					]
+				},
+				{
+					label : "Jumlah Anak Yang Sekolah",
+					opsiJawaban : [
+						{nilai:"5", label:"4 Anak"},
+						{nilai:"4", label:"3 Anak"},
+						{nilai:"3", label:"2 Anak"},
+						{nilai:"2", label:"1 Anak"}
+					]
+				},
+				{
+					label : "Anak Yang Putus Sekolah",
+					opsiJawaban : [
+						{nilai:"5", label:"3 Anak"},
+						{nilai:"4", label:"2 Anak"},
+						{nilai:"3", label:"1 Anak"},
+						{nilai:"2", label:"Tidak Ada"}
+					]
+				},
+				{
+					label : "Istri Hamil",
+					opsiJawaban : [
+						{nilai:"5", label:"Usia 8-9 bulan"},
+						{nilai:"4", label:"Usia 6-7 bulan"},
+						{nilai:"3", label:"Usia 4-5 bulan"},
+						{nilai:"2", label:"Usia 1-3 bulan"}
+					]
+				},
+				{
+					label : "Status Penerima Manfaat dalam Keluarga",
+					opsiJawaban : [
+						{nilai:"5", label:"Yatim Piatu"},
+						{nilai:"4", label:"Yatim / Piatu"},
+						{nilai:"3", label:"Anak Angkat"},
+						{nilai:"2", label:"Anak Kandung"}
+					]
+				},
+			]
+		},
+		{
+			judulSub:"DATA KONDISI RUMAH PENERIMA MANFAAT",
+			pertanyaanSurvey : [
+				{
+					label : "Kepemilikan Rumah",
+					opsiJawaban : [
+						{nilai:"5", label:"Menumpang"},
+						{nilai:"4", label:"Kontrak"},
+						{nilai:"3", label:"Keluarga"},
+						{nilai:"2", label:"Sendiri"}
+					]
+				},
+				{
+					label : "Luas Rumah dan Lantai",
+					opsiJawaban : [
+						{nilai:"5", label:"4x4 Meter"},
+						{nilai:"4", label:"4x5 Meter"},
+						{nilai:"3", label:"4x6 Meter"},
+						{nilai:"2", label:"6x6 Meter"}
+					]
+				},
+				{
+					label : "Dinding Rumah",
+					opsiJawaban : [
+						{nilai:"5", label:"Papan"},
+						{nilai:"4", label:"Semi Permanen"},
+						{nilai:"3", label:"Permanen"}
+					]
+				},
+				{
+					label : "Lantai",
+					opsiJawaban : [
+						{nilai:"5", label:"Kayu Papan"},
+						{nilai:"4", label:"Semi Permanen"},
+						{nilai:"3", label:"Semen"},
+						{nilai:"2", label:"Keramik"}
+					]
+				},
+				{
+					label : "Atap",
+					opsiJawaban : [
+						{nilai:"5", label:"Rumbai / daun"},
+						{nilai:"4", label:"Daun dan Seng"},
+						{nilai:"3", label:"Seng"},
+						{nilai:"2", label:"Genting dan Cor"}
+					]
+				},
+				{
+					label : "Dapur",
+					opsiJawaban : [
+						{nilai:"5", label:"Tungku / Kayu"},
+						{nilai:"4", label:"Kompor Minyak"},
+						{nilai:"3", label:"Kompor Gas"},
+						{nilai:"2", label:"Kompor Listrik"}
+					]
+				},
+				{
+					label : "Kursi",
+					opsiJawaban : [
+						{nilai:"5", label:"Lesehan"},
+						{nilai:"4", label:"Kayu"},
+						{nilai:"3", label:"Sofa Sederhana"},
+						{nilai:"2", label:"Jati / Sofa Mahal"}
+					]
+				},
+				{
+					label : "Sumber Air",
+					opsiJawaban : [
+						{nilai:"5", label:"Sungai / Parit"},
+						{nilai:"4", label:"Kolam"},
+						{nilai:"3", label:"BOR"},
+						{nilai:"2", label:"PDAM"}
+					]
+				},
+				{
+					label : "Tempat MCK",
+					opsiJawaban : [
+						{nilai:"5", label:"Sungai / Parit"},
+						{nilai:"4", label:"MCK Umum"},
+						{nilai:"3", label:"Satu Keluarga Bersama"},
+						{nilai:"2", label:"Sendiri"}
+					]
+				},
+				{
+					label : "Penerangan / Listrik",
+					opsiJawaban : [
+						{nilai:"5", label:"Pelita"},
+						{nilai:"4", label:"Listrik Bersama"},
+						{nilai:"3", label:"Numpang"},
+						{nilai:"2", label:"Sendiri"}
+					]
+				},
+				{
+					label : "Daya Listrik",
+					opsiJawaban : [
+						{nilai:"5", label:"450 Watt"},
+						{nilai:"4", label:"900 Watt"},
+						{nilai:"3", label:"1300 Watt"}
+					]
+				},
+				{
+					label : "Lokasi Rumah",
+					opsiJawaban : [
+						{nilai:"5", label:"Perkampungan Kumuh"},
+						{nilai:"4", label:"Bantara Sungai / Kali"},
+						{nilai:"3", label:"Perkampungan Biasa"},
+						{nilai:"2", label:"Kompleks"}
+					]
+				},
+				{
+					label : "Tata Letak Bangunan Rumah",
+					opsiJawaban : [
+						{nilai:"5", label:"Tidak Teratur"},
+						{nilai:"4", label:"Kurang Teratur"},
+						{nilai:"3", label:"Teratur"},
+						{nilai:"2", label:"Sangat Teratur"}
 					]
 				}
 			]
 		},
 		{
-			judulSub:"Sub Judul 1",
+			judulSub:"KEPEMILIKAN BARANG",
 			pertanyaanSurvey : [
 				{
-					label : "Pertanyaan 1, sub 1",
+					label : "Elektronik",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 1 Sub 1"},
-						{nilai:"2", label:"Opsi B Pertanyaan 1 Sub 1"},
-						{nilai:"3", label:"Opsi C Pertanyaan 1 Sub 1"},
-						{nilai:"4", label:"Opsi D Pertanyaan 1 Sub 1"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 1 Sub 1"}
+						{nilai:"5", label:"Tidak ada"},
+						{nilai:"4", label:"Radio"},
+						{nilai:"3", label:"TV"},
+						{nilai:"2", label:"Kulkas"}
 					]
 				},
 				{
-					label : "Pertanyaan 2, sub 1",
+					label : "Kendaraan",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 2 Sub 1"},
-						{nilai:"2", label:"Opsi B Pertanyaan 2 Sub 1"},
-						{nilai:"3", label:"Opsi C Pertanyaan 2 Sub 1"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 2 Sub 1"}
+						{nilai:"5", label:"Tidak ada"},
+						{nilai:"4", label:"Sepeda"},
+						{nilai:"3", label:"Motor Maksimal 1"},
+						{nilai:"2", label:"Mobil"}
 					]
 				},
 				{
-					label : "Pertanyaan 3, sub 1",
+					label : "Alat Komunikasi",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 3 Sub 1"},
-						{nilai:"2", label:"Opsi B Pertanyaan 3 Sub 1"},
-						{nilai:"3", label:"Opsi C Pertanyaan 3 Sub 1"},
-						{nilai:"4", label:"Opsi D Pertanyaan 3 Sub 1"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 3 Sub 1"}
+						{nilai:"5", label:"Tidak Ada"},
+						{nilai:"4", label:"HP Lama"},
+						{nilai:"3", label:"HP Baru"},
+						{nilai:"2", label:"HP > dari 2 buah"}
+					]
+				},
+				{
+					label : "Ternak",
+					opsiJawaban : [
+						{nilai:"5", label:"Tidak ada"},
+						{nilai:"4", label:"Unggas"},
+						{nilai:"3", label:"Kambing"},
+						{nilai:"2", label:"Sapi"}
+					]
+				},
+				{
+					label : "Perhiasan / Emas",
+					opsiJawaban : [
+						{nilai:"5", label:"Tidak ada"},
+						{nilai:"4", label:"Cincin Nikah"},
+						{nilai:"3", label:"Anting-anting"},
+						{nilai:"2", label:"Kalung / Gelang"}
+					]
+				},
+				{
+					label : "Tanah",
+					opsiJawaban : [
+						{nilai:"5", label:"Tidak ada"},
+						{nilai:"4", label:"Satu Petak"},
+						{nilai:"3", label:"1/2 Ha"},
+						{nilai:"2", label:"Lebih dari 1 Ha"}
 					]
 				}
 			]
 		},
 		{
-			judulSub:"Sub Judul 2",
+			judulSub:"KESEHATAN KELUARGA",
 			pertanyaanSurvey : [
 				{
-					label : "Pertanyaan 1, sub 2",
+					label : "Kemampuan Berobat",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 1 Sub 2"},
-						{nilai:"2", label:"Opsi B Pertanyaan 1 Sub 2"},
-						{nilai:"3", label:"Opsi C Pertanyaan 1 Sub 2"},
-						{nilai:"4", label:"Opsi D Pertanyaan 1 Sub 2"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 1 Sub 2"}
+						{nilai:"5", label:"Tidak Mampu"},
+						{nilai:"4", label:"Herbal Sendiri"},
+						{nilai:"3", label:"Herbal Dukun"},
+						{nilai:"2", label:"Puskesmas / RS"}
 					]
 				},
 				{
-					label : "Pertanyaan 2, sub 1",
+					label : "Pola Penyakit dalam Keluarga dalam Sebulan",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 2 Sub 2"},
-						{nilai:"2", label:"Opsi B Pertanyaan 2 Sub 2"},
-						{nilai:"3", label:"Opsi C Pertanyaan 2 Sub 2"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 2 Sub 2"}
+						{nilai:"5", label:"4 Kali"},
+						{nilai:"4", label:"1-4 Kali"},
+						{nilai:"3", label:"2-3 Kali"},
+						{nilai:"2", label:"1-2 Kali"}
 					]
 				},
 				{
-					label : "Pertanyaan 3, sub 1",
+					label : "Terdapat Penyakit Parah / Memerlukan Pembiayaan Besar",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 3 Sub 2"},
-						{nilai:"2", label:"Opsi B Pertanyaan 3 Sub 2"},
-						{nilai:"3", label:"Opsi C Pertanyaan 3 Sub 2"},
-						{nilai:"4", label:"Opsi D Pertanyaan 3 Sub 2"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 3 Sub 2"}
+						{nilai:"5", label:"Dokter / Spesialis"},
+						{nilai:"4", label:"Dokter Umum / Puskesmas"},
+						{nilai:"3", label:"Herbal / Dukun"},
+						{nilai:"2", label:"Herbal Sendiri"}
 					]
 				},
 				{
-					label : "Pertanyaan 4, sub 1",
+					label : "Kondisi Ibu Hamil",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 4 Sub 2"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 4 Sub 2"}
+						{nilai:"5", label:"Resiko Tinggi"},
+						{nilai:"4", label:"Resiko Sedang"},
+						{nilai:"3", label:"Normal"}
 					]
 				},
 				{
-					label : "Pertanyaan 5, sub 1",
+					label : "Kondisi Gizi Ibu Hamil",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 5 Sub 2"},
-						{nilai:"2", label:"Opsi B Pertanyaan 5 Sub 2"},
-						{nilai:"3", label:"Opsi C Pertanyaan 5 Sub 2"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 5 Sub 2"}
+						{nilai:"5", label:"Gizi Buruk"},
+						{nilai:"4", label:"Gizi Kurang"},
+						{nilai:"3", label:"Gizi Baik"},
+						{nilai:"2", label:"Gizi Lebih"}
 					]
 				},
 				{
-					label : "Pertanyaan 6, sub 1",
+					label : "Berat Badan di Bawah Standar",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 6 Sub 2"},
-						{nilai:"2", label:"Opsi B Pertanyaan 6 Sub 2"},
-						{nilai:"3", label:"Opsi C Pertanyaan 6 Sub 2"},
-						{nilai:"4", label:"Opsi D Pertanyaan 6 Sub 2"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 6 Sub 2"}
+						{nilai:"5", label:"Tidak Normal"},
+						{nilai:"4", label:"Sedang"},
+						{nilai:"3", label:"Normal"}
+					]
+				},
+				{
+					label : "Imunisasi / ke Posyandu",
+					opsiJawaban : [
+						{nilai:"5", label:"Tidak Pernah"},
+						{nilai:"4", label:"1 Kali / 9 Bulan"},
+						{nilai:"3", label:"3 Kali / 9 Bulan"},
+						{nilai:"2", label:"Setiap Bulan"}
+					]
+				},
+				{
+					label : "Pola Makan sehari-hari (nasi)",
+					opsiJawaban : [
+						{nilai:"5", label:"1 Kali Sehari"},
+						{nilai:"4", label:"2 Kali Sehari"},
+						{nilai:"3", label:"3 Kali Sehari"},
+						{nilai:"2", label:"Lengkap Gizi"}
 					]
 				}
 			]
 		},
 		{
-			judulSub:"Sub Judul 3",
+			judulSub:"TANGGUNGAN KEBUTUHAN HIDUP",
 			pertanyaanSurvey : [
 				{
-					label : "Pertanyaan 1, sub 2",
+					label : "Biaya Listrik",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 1 Sub 3"},
-						{nilai:"2", label:"Opsi B Pertanyaan 1 Sub 3"},
-						{nilai:"3", label:"Opsi C Pertanyaan 1 Sub 3"},
-						{nilai:"4", label:"Opsi D Pertanyaan 1 Sub 3"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 1 Sub 3"}
+						{nilai:"5", label:"20.000 - 30.000"},
+						{nilai:"4", label:"30.000 - 50.000"},
+						{nilai:"3", label:"50.000 - 70.000"},
+						{nilai:"2", label:"70.000 - 100.000"}
 					]
 				},
 				{
-					label : "Pertanyaan 2, sub 1",
+					label : "Tanggungan Utang",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 2 Sub 3"},
-						{nilai:"2", label:"Opsi B Pertanyaan 2 Sub 3"},
-						{nilai:"3", label:"Opsi C Pertanyaan 2 Sub 3"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 2 Sub 3"}
+						{nilai:"5", label:"1 Juta"},
+						{nilai:"4", label:"700.000"},
+						{nilai:"3", label:"500.000"},
+						{nilai:"2", label:"300.000"}
 					]
 				},
 				{
-					label : "Pertanyaan 3, sub 1",
+					label : "Bayar Telpon / HP",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 3 Sub 3"},
-						{nilai:"2", label:"Opsi B Pertanyaan 3 Sub 3"},
-						{nilai:"3", label:"Opsi C Pertanyaan 3 Sub 3"},
-						{nilai:"4", label:"Opsi D Pertanyaan 3 Sub 3"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 3 Sub 3"}
+						{nilai:"5", label:"5.000 - 10.000"},
+						{nilai:"4", label:"15.000 - 20.000"},
+						{nilai:"3", label:"25.000 - 50.000"},
+						{nilai:"2", label:"60.000 - 100.000"}
+					]
+				},
+				{
+					label : "Transportasi",
+					opsiJawaban : [
+						{nilai:"5", label:"150.000"},
+						{nilai:"4", label:"300.000"},
+						{nilai:"3", label:"450.000"},
+						{nilai:"2", label:"600.000"}
+					]
+				},
+				{
+					label : "Biaya Pendidikan",
+					opsiJawaban : [
+						{nilai:"5", label:"30.000"},
+						{nilai:"4", label:"60.000 - 100.000"},
+						{nilai:"3", label:"110.000 - 200.000"},
+						{nilai:"2", label:"210.000 - 300.000"}
 					]
 				}
 			]
 		},
 		{
-			judulSub:"Sub Judul 4",
+			judulSub:"INDIKATOR KEIMANAN",
 			pertanyaanSurvey : [
 				{
-					label : "Pertanyaan 1, sub 4",
+					label : "Kebiasaan Patologis (miraz, judi, zina, narkoba",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 1 sub 4"},
-						{nilai:"2", label:"Opsi B Pertanyaan 1 sub 4"},
-						{nilai:"3", label:"Opsi C Pertanyaan 1 sub 4"},
-						{nilai:"4", label:"Opsi D Pertanyaan 1 sub 4"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 1 sub 4"}
+						{nilai:"5", label:"Tidak Pernah"},
+						{nilai:"4", label:"Pernah Tapi Insyaf"},
+						{nilai:"3", label:"Jarang"},
+						{nilai:"2", label:"Sering"}
 					]
 				},
 				{
-					label : "Pertanyaan 2, sub 4",
+					label : "Pola Sholat Pada Anggota Keluarga",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 2 sub 4"},
-						{nilai:"2", label:"Opsi B Pertanyaan 2 sub 4"},
-						{nilai:"3", label:"Opsi C Pertanyaan 2 sub 4"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 2 sub 4"}
+						{nilai:"5", label:"Teratur"},
+						{nilai:"4", label:"Tidak Teratur"},
+						{nilai:"3", label:"Kadang-kadang"},
+						{nilai:"2", label:"Tidak Pernah"}
 					]
 				},
 				{
-					label : "Pertanyaan 3, sub 4",
+					label : "Rajin Mengikuti Kajian",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 3 sub 4"},
-						{nilai:"2", label:"Opsi B Pertanyaan 3 sub 4"},
-						{nilai:"3", label:"Opsi C Pertanyaan 3 sub 4"},
-						{nilai:"4", label:"Opsi D Pertanyaan 3 sub 4"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 3 sub 4"}
+						{nilai:"5", label:"Setiap Hari"},
+						{nilai:"4", label:"4x Dalam Seminggu"},
+						{nilai:"3", label:"Sekali Dalam Seminggu"},
+						{nilai:"2", label:"Tidak Pernah"}
 					]
 				},
 				{
-					label : "Pertanyaan 4, sub 4",
+					label : "Istri anak gadis memakai jilbab",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 4 sub 4"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 4 sub 4"}
+						{nilai:"5", label:"Memakai"},
+						{nilai:"4", label:"Kadang-kadang"},
+						{nilai:"3", label:"Tidak Pernah"}
 					]
 				},
 				{
-					label : "Pertanyaan 5, sub 4",
+					label : "Merokok",
 					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 5 sub 4"},
-						{nilai:"2", label:"Opsi B Pertanyaan 5 sub 4"},
-						{nilai:"3", label:"Opsi C Pertanyaan 5 sub 4"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 5 sub 4"}
-					]
-				},
-				{
-					label : "Pertanyaan 6, sub 4",
-					opsiJawaban : [
-						{nilai:"1", label:"Opsi A Pertanyaan 6 sub 4"},
-						{nilai:"2", label:"Opsi B Pertanyaan 6 sub 4"},
-						{nilai:"3", label:"Opsi C Pertanyaan 6 sub 4"},
-						{nilai:"4", label:"Opsi D Pertanyaan 6 sub 4"},
-						{nilai:"5", label:"Opsi E Pertanyaan 6 sub 4"},
-						{nilai:"6", label:"Opsi F Pertanyaan 6 sub 4"},
-						{nilai:"0", label:"Opsi Default Pertanyaan 6 sub 4"}
+						{nilai:"5", label:"Tidak Pernah"},
+						{nilai:"4", label:"Pernah Tapi Insyaf"},
+						{nilai:"3", label:"Merokok dan berjanji berhenti"}
 					]
 				}
 			]
