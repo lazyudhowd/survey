f77882a86ab94ed3281d62dc195d2d7031304ecd
lazyudhowd <154648790+lazyudhowd@users.noreply.github.com>
2023-12-26 16:43:03 +0700

    Update function.js
    Menambahkan beberapa function
    Salah satunya adalah function "totalSemuaSurvey" yang akan mengkalkulasi semua nilai survey dan langsung mengupdate tampilan skor dan rekomendasi survey

--- function.js
+++ function.js
@@ -7,3 +7,33 @@ function printSurvey(){
 		});
 	});
 }
+
+function totalSemuaSurvey(){
+	let totalNilai = 0;
+
+	for(let ii=0; ii<registerSurvey.length; ii++){
+		
+		let radio = document.getElementsByName(registerSurvey[ii]);
+
+		for(let ij=0; ij<radio.length; ij++){
+			if(radio[ij].checked){
+				totalNilai += parseInt(radio[ij].value);
+			}
+		}
+	}
+
+	let rekomendasiIndex = 0
+
+	for(let ii=0; ii<dataRekomendasi.length; ii++){
+		if(dataRekomendasi[ii].min <= totalNilai){
+			if(dataRekomendasi[ii].max >= totalNilai){
+				rekomendasiIndex = ii;
+				break;
+			}
+		}
+	}
+
+	document.getElementById("labelSkor").innerText = "SKOR "+totalNilai;
+
+	document.getElementById("labelRekomendasi").innerText = dataRekomendasi[rekomendasiIndex].label;
+}
