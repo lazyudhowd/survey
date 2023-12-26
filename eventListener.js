cefc885cf54c0f3a00428e3cd5ad4ed038289204
lazyudhowd <154648790+lazyudhowd@users.noreply.github.com>
2023-12-26 16:41:46 +0700

    Update eventListener.js
    Menambahkan event listener untuk setiap radio sehingga ketika nilai radio berubah, akan langsung mengubah skor dan rekomendasi hasil survey

--- eventListener.js
+++ eventListener.js
@@ -1,1 +1,11 @@
 document.getElementById("btnPrintSurvey").addEventListener("click",printSurvey,false);
+
+// menambahkan function
+for(let ii=0; ii<registerSurvey.length; ii++){
+	
+	let radio = document.getElementsByName(registerSurvey[ii]);
+
+	for(let ij=0; ij<radio.length; ij++){
+		radio[ij].addEventListener("change",totalSemuaSurvey)
+	}
+}
